160a5d85419aeacd0c7f483e9214f24cd4f3b267
Arkadiy <99073279+Deflnam@users.noreply.github.com>
2026-05-24 15:36:15 +0300

    add main program with file reading

--- burukov.arkadiy/T3/main.cpp
+++ burukov.arkadiy/T3/main.cpp
@@ -1,6 +1,37 @@
+#include <algorithm>
+#include <fstream>
 #include <iostream>
+#include <iterator>
+#include <vector>
 
-int main()
+#include "cli.hpp"
+#include "polygon.hpp"
+
+int main(int argc, char* argv[])
 {
-    return 0;
+  if (argc != 2)
+  {
+    std::cerr << "Usage: " << argv[0] << " <filename>\n";
+    return 1;
+  }
+
+  std::ifstream file(argv[1]);
+
+  if (!file)
+  {
+    std::cerr << "Cannot open file: " << argv[1] << '\n';
+    return 1;
+  }
+
+  std::vector<burukov::Polygon> polygons;
+  burukov::readData(file, polygons);
+  burukov::Command::polygons = &polygons;
+
+  std::vector<burukov::Command> commands;
+  std::copy(
+    std::istream_iterator<burukov::Command>{std::cin},
+    std::istream_iterator<burukov::Command>{},
+    std::back_inserter(commands));
+
+  return 0;
 }
